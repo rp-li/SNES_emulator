e98d83cc220729efd49daeece398553ea071b2dc
ruopingli <ruopingli@hotmail.com>
2016-04-23 12:51:22 -0400

    Signed-off-by: ruopingli <ruopingli@hotmail.com>

--- snes.py
+++ snes.py
@@ -11,17 +11,17 @@ class cpu:
         self.speed=speed
         self.cycles=0
         #CPU REGISTERS
-        self.reg_A='0000000000000000'  #accumulator
-        self.reg_X='0000000000000000'  #index
-        self.reg_Y='0000000000000000'  #index
-        self.reg_S='0000000000000000'  #stack pointer
-        self.reg_DB='00000000'   #data bank
-        self.reg_D='0000000000000000'     #direct page
-        self.reg_PB='00000000'     #program bank
-        self.reg_P='00000000'     #processor status flags
-        self.reg_PC='0000000000000000'   #program counter (memory address)
+        self.reg_A='0000'  #accumulator(hex)
+        self.reg_X='0000'  #index(hex)
+        self.reg_Y='0000'  #index(hex)
+        self.reg_S='0000'  #stack pointer(hex)
+        self.reg_DB='00'   #data bank(hex)
+        self.reg_D='0000'     #direct page(hex)
+        self.reg_PB='00'     #program bank(hex)
+        self.reg_P='00000000'     #processor status flags (binary)
+        self.reg_PC='0000'   #program counter (memory address)
 
-    def debug(self):
+    def show(self):
         print 'Accumulator register: ', self.reg_A
         print 'X index register: ', self.reg_A
         print 'Y index register: ', self.reg_A
@@ -33,8 +33,12 @@ class cpu:
         print 'Program counter register: ', self.reg_A
 
     def readrom(self, addr):
-        self.romaddr=2*addr
-        return rom.buffer[self.romaddr:self.romaddr+2]
+        #self.reg_PC=
+        self.romindex=2*(int(addr, 16))
+        return rom.buffer[self.romindex:self.romindex+2]
+    
+    def readromindex(self, index):
+        return rom.buffer[index:index+2]
         
     def run(self, bytecode):
         if bytecode=='78':
@@ -43,9 +47,9 @@ class cpu:
             self.setflag('I')
             
         elif bytecode=='9C':
+            arg=cpu.readromindex(self.romindex)+cpu.readrom(self.romindex+1)
             if self.debug==1:
-                print 'STZ(absolute)'
-            arg=cpu.readrom(self.romaddr+1)+cpu.readrom(self.romaddr+2)
+                print 'STZ(absolute) at ', arg
             mem.write(arg, '00')
 
     def setflag(self,flag):
