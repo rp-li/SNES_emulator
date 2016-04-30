4f8e1d5ed46ae075553ab0f770e50059b09f515c
ruopingli <ruopingli@hotmail.com>
2016-04-30 19:03:02 -0400

    Signed-off-by: ruopingli <ruopingli@hotmail.com>

--- snes.py
+++ snes.py
@@ -1,15 +1,23 @@
 import numpy as np
 import binascii
 import sys
+import threading
 
 ROMPATH="../SMW_rom.sfc"
 #ROMPATH="../test.txt"
+
+def dec(addr):
+    if type(addr)==int:
+        return addr
+    elif type(addr)==str:
+        return int(addr,16)
 
 class cpu:
     def __init__(self, speed):
         self.debug=1
         self.speed=speed
         self.cycles=0
+        self.isrunning=0
         #CPU REGISTERS
         self.reg_A='0000'  #accumulator(hex)
         self.reg_X='0000'  #index(hex)
@@ -20,37 +28,53 @@ class cpu:
         self.reg_PB='00'     #program bank(hex)
         self.reg_P='00000000'     #processor status flags (binary)
         self.reg_PC='0000'   #program counter (memory address)
+        print 'CPU Initialized'
+
+    def tick(self):
+        self.reg_PC=str(hex(dec(cpu.reg_PC)+1)[2:]) #incrementing the program counter
+        self.cycles+=1
+        
+    def start(self):
+        self.reg_PC=rom.reset_vector
+        self.isrunning=1
+        while self.isrunning==1:
+            self.run(mem.read(self.reg_PB,self.reg_PC,self.reg_PC))
+
+            if self.cycles>20:
+                self.isrunning=0
 
     def show(self):
         print 'Accumulator register: ', self.reg_A
-        print 'X index register: ', self.reg_A
-        print 'Y index register: ', self.reg_A
-        print 'Stack Pointer register: ', self.reg_A
-        print 'Data Bank register: ', self.reg_A
-        print 'Direct Page register: ', self.reg_A
-        print 'Program Bank register: ', self.reg_A
-        print 'Processor Flag register: ', self.reg_A
-        print 'Program counter register: ', self.reg_A
-
-    def readrom(self, addr):
-        #self.reg_PC=
-        self.romindex=2*(int(addr, 16))
-        return rom.buffer[self.romindex:self.romindex+2]
-    
-    def readromindex(self, index):
-        return rom.buffer[index:index+2]
+        print 'X index register: ', self.reg_X
+        print 'Y index register: ', self.reg_Y
+        print 'Stack Pointer register: ', self.reg_S
+        print 'Data Bank register: ', self.reg_DB
+        print 'Direct Page register: ', self.reg_D
+        print 'Program Bank register: ', self.reg_PB
+        print 'Processor Flag register: ', self.reg_P
+        print 'Program counter register: ', self.reg_PC
         
     def run(self, bytecode):
         if bytecode=='78':
             if self.debug==1:
-                print 'SEI'
+                print self.cycles,'SEI'
+            self.tick()
             self.setflag('I')
             
-        elif bytecode=='9C':
-            arg=cpu.readromindex(self.romindex)+cpu.readrom(self.romindex+1)
+        elif bytecode=='9c':
             if self.debug==1:
-                print 'STZ(absolute) at ', arg
-            mem.write(arg, '00')
+                print self.cycles, 
+            self.tick()
+            temp1=mem.read(self.reg_PB,hex(dec(self.reg_PC))[2:],hex(dec(self.reg_PC))[2:])
+            self.tick()
+            temp2=mem.read(self.reg_PB,hex(dec(self.reg_PC))[2:],hex(dec(self.reg_PC))[2:])
+            if self.debug==1:
+                print 'STZ(absolute) at ', self.reg_PB, temp2+temp1
+            self.tick()
+            mem.write(self.reg_PB, temp2+temp1, '00')
+
+        else:
+            print 'CPU error: unknown opcode'
 
     def setflag(self,flag):
         if 'N' in flag:
@@ -97,23 +121,67 @@ class cpu:
 
 class mem:
     def __init__(self):
-        self.mem={}
-    def write(self, addr, val):
-        if type(addr)==str and type(val)==str:
-            self.mem[addr]=str(val)
-    def read(self, addr):
-        if type(addr)==str:
-            if not addr in self.mem:
-                return '00'
-            return self.mem[addr]
+        #00 to FF memory banks
+        print 'Building SNES memory map...'
+        self.mem=[]
+        for i in range(256):
+            self.mem.append([])
+            for j in range(65536):
+                self.mem[i].append('00')
+        if rom.mode=='hiROM':
+            for b in range(dec('c0'),dec('ff')):
+                self.write(b,'0000',rom.read(65536*(b-192),65536*(b-192)+dec('ffff')))                                             
+            for b in range(dec('00'),dec('1f')):
+                self.write(b,'8000',self.read(b+dec('c0'),'0000','7fff'))
+                           
+        elif rom.mode=='loROM':
+            pass
+
+    def read(self, bank, addr_start, addr_end):
+        out=''
+        for i in range(dec(addr_start),dec(addr_end)+1):
+            out=out+str(self.mem[dec(bank)][i])
+        return out
+        
+    def write(self, bank, addr, val):
+        addr=dec(addr)
+        if type(bank)==int:
+            for i in range(len(val)/2):
+                self.mem[bank][addr]=val[2*i:2*i+2]
+                addr+=1
+        elif type(bank)==str:
+            
+            for i in range(len(val)/2):
+                self.mem[dec(bank)][addr]=val[2*i:2*i+2]
+                addr+=1
 
 class rom:
-    def __init__(self):
+    def __init__(self): 
         with open(ROMPATH, 'rb') as r:
             content=r.read()
         self.buffer=binascii.hexlify(content)
-        self.buffer=self.buffer.upper()
+        self.header=self.read('000015', '000015')
+        if self.header=='20':
+            rom.mode='loROM'
+        elif self.header=='21':
+            rom.mode='hiROM'
+            self.title=self.read('7FC0','7FD0').decode('hex')
+        self.reset_vector=self.read('7FFD','7FFD')+self.read('7FFC','7FFC')
+
+    def read(self, start_addr, end_addr):
+        if type(start_addr)==str and type(end_addr)==str:
+            if len(self.buffer)<=2*dec(start_addr):
+                return '00'
+            return self.buffer[2*(int(start_addr, 16)):2*(int(end_addr, 16))+2]
+        elif type(start_addr)==int and type(end_addr)==int:
+            if len(self.buffer)<=2*start_addr:
+                return '00'
+            return self.buffer[2*start_addr:2*end_addr+2]
         
 rom=rom()
 cpu=cpu(3580000)
 mem=mem()
+
+#boot sequence
+print 'SNES started'
+cpu.start()
